6b03f09c5f4e3691e91c70bf3204693ca65f562a
wrex1k <156697493+wrex1k@users.noreply.github.com>
2025-11-05 20:59:01 +0100

    spring boot setup

---
+++ src/main/java/com/pizzeriadiroma/pizzeria/PizzeriaDiRomaApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeriadiroma.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaDiRomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaDiRomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzeriadiroma/pizzeria/PizzeriaDiRomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeriadiroma.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaDiRomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
